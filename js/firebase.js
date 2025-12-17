b798e71cb997d37c799aa8b5d9bb7476fc45fafa
hamzaalaajwjw <kaboosyt101@gmail.com>
2025-12-17 16:56:10 +0300

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,25 @@
-// Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyAl3XunFOwHpGw-4_VYyETMtoLgk4mnRpQ",
-  authDomain: "a3len-3ad54.firebaseapp.com",
-  databaseURL: "https://a3len-3ad54-default-rtdb.firebaseio.com",
-  projectId: "a3len-3ad54",
-  storageBucket: "a3len-3ad54.firebasestorage.app",
-  messagingSenderId: "767338034080",
-  appId: "1:767338034080:web:801d77fb74c0aa56e92ac5"
-};
+<!-- 1️⃣ Firebase SDK -->
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+<!-- 2️⃣ Firebase Config -->
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyAl3XunFOwHpGw-4_VYyETMtoLgk4mnRpQ",
+    authDomain: "a3len-3ad54.firebaseapp.com",
+    databaseURL: "https://a3len-3ad54-default-rtdb.firebaseio.com",
+    projectId: "a3len-3ad54",
+    storageBucket: "a3len-3ad54.firebasestorage.app",
+    messagingSenderId: "767338034080",
+    appId: "1:767338034080:web:801d77fb74c0aa56e92ac5"
+  };
 
-// تعريف قاعدة البيانات والتخزين
-const db = firebase.database();
-const storage = firebase.storage();
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // تعريف قاعدة البيانات
+  const db = firebase.database();
+</script>
+
+<!-- 3️⃣ App Logic -->
+<script src="js/app.js"></script>
