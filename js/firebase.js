db1674a6dc49a60e2579c427bed5c8f958d908c3
hamzaalaajwjw <kaboosyt101@gmail.com>
2025-12-17 16:41:23 +0300

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,7 +2,7 @@
 const firebaseConfig = {
   apiKey: "AIzaSyAl3XunFOwHpGw-4_VYyETMtoLgk4mnRpQ",
   authDomain: "a3len-3ad54.firebaseapp.com",
-  databaseURL: "https://a3len-3ad54-default-rtdb.firebaseio.com",
+  databaseURL: "https://a3len-3ad54-default-rtdb.firebaseio.com/",
   projectId: "a3len-3ad54",
   storageBucket: "a3len-3ad54.firebasestorage.app",
   messagingSenderId: "767338034080",
@@ -13,4 +13,5 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.database();
+
 const storage = firebase.storage();
