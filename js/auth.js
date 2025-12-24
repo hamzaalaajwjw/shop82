fd0910e6f323982acf159987587de2c5e4051212
hamzaalaajwjw <kaboosyt101@gmail.com>
2025-12-24 20:13:00 +0300

    Delete js directory

--- js/auth.js
+++
@@ -1,7 +0,0 @@
-import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-
-window.login = async function () {
-  await signInWithEmailAndPassword(auth, email.value, password.value);
-  location.href = "index.html";
-};
